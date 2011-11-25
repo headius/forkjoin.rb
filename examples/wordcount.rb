96ae7dabae0e975459307cf9d1c5abc2fe4c7008
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-11-25 18:55:42 +0900

    Update wordcount sample for benchmarking

--- examples/wordcount.rb
+++ examples/wordcount.rb
@@ -1,17 +1,28 @@
 require 'forkjoin'
+require 'benchmark'
 
-pool = ForkJoin::Pool.new
+file = ARGV.shift
 
-map_futures = pool.invoke_all(
-  ARGF.each_line.map{|line| ->{line.split.map{|word| [word,1]}}}
-)
-counts = map_futures.map(&:get).inject({}) {|map, value|
-  value.each {|k,v| (map[k] ||= []) << v}
-  map
-}
-reduced_futures = pool.invoke_all(
-  counts.map{|k, vs| ->{[k, vs.size]}}
-)
-reduced_futures.map(&:get).each{|value|
-  puts "%s %d\n" % value
-}
+5.times do
+  Benchmark.bm do |bm|
+    bm.report do
+      pool = ForkJoin::Pool.new
+      map_futures = nil
+      File.open(file) do |f|
+        map_futures = pool.invoke_all(
+          f.each_line.map { |line| -> { line.split.map { |word| [word,1] } } }
+        )
+      end
+      counts = map_futures.map(&:get).inject({}) { |map, value|
+        value.each { |k, v| (map[k] ||= []) << v }
+        map
+      }
+      reduced_futures = pool.invoke_all(
+        counts.map { |k, vs| -> { [k, vs.size] } }
+      )
+      # reduced_futures.map(&:get).each{|value|
+      #   puts "%s %d\n" % value
+      # }
+    end
+  end
+end
